66a46bfe6c7bbd5af86e4d61d4d63ada6bd3c288
kebyang <kebyang1511@yahoo.com>
2015-01-30 13:02:52 +0000

    production configuration

--- protected/config/main.php
+++ protected/config/main.php
@@ -49,10 +49,10 @@ return array(
         ),
         // uncomment the following to use a MySQL database
         'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=hlrv2',
+            'connectionString' => 'mysql:host=localhost;dbname=freedebt_hlr',
             'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => 'root',
+            'username' => 'freedebt_hlr',
+            'password' => 'hitman052529',
             'charset' => 'utf8',
         ),
         'errorHandler' => array(
